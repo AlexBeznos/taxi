0f7b0205a583ac002679842dd0c61ce88c163fd9
Alex Beznos <beznosa@yahoo.com>
2015-03-12 20:25:34 +0200

    front controller

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def index
+  end
+
+  def calculate
+  end
+
+  def call
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'static#index'
+  get 'static/index'
+  get 'static/calculate'
+  get 'static/call'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
