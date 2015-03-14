5c9d29808858ecca393205a389941a075c75846f
Alex Beznos <beznosa@yahoo.com>
2015-03-14 04:32:14 +0200

    add delayed jobs, fix bugs

--- app/models/services/notification.rb
+++ app/models/services/notification.rb
@@ -4,32 +4,33 @@ class Notification
     send_email_message(order)
   end
 
-  private
-  def send_vk_message(order)
-    begin
+  class << self
+    def send_vk_message(order)
+      begin
 
-      client = VkontakteApi::Client.new(ENV['VK_ACCESS_TOKEN'])
-      message = "Долбоеб, зайди на сайт там у тебя такси заказали, ёпт.<br>Ехать надо от #{order.from_street} #{order.from_house} и до #{order.to_street} #{order.to_house}<br>Заебал со своей хуетой!"
-      client.messages.send(domain: ENV['VK_DOMAIN'], message: message)
+        client = VkontakteApi::Client.new(ENV['VK_ACCESS_TOKEN'])
+        message = "Долбоеб, зайди на сайт там у тебя такси заказали, ёпт.<br>Ехать надо от #{order.from_street} #{order.from_house} и до #{order.to_street} #{order.to_house}<br>Заебал со своей хуетой!"
+        client.messages.send(domain: ENV['VK_DOMAIN'], message: message)
 
-    rescue VkontakteApi::Error => e
+      rescue VkontakteApi::Error => e
 
-      if e.error_code == 14
-        client_captcha = DeathByCaptcha.new(ENV['CAPTCHA_USER'], ENV['CAPTCHA_PASS'], :http)
-        captcha = client_captcha.decode(url: e.captcha_img)
+        if e.error_code == 14
+          client_captcha = DeathByCaptcha.new(ENV['CAPTCHA_USER'], ENV['CAPTCHA_PASS'], :http)
+          captcha = client_captcha.decode(url: e.captcha_img)
 
-        client.messages.send(domain: ENV['VK__DOMAIN'],
-                             message: message,
-                             captcha_sid: e.captcha_sid,
-                             captcha_key: captcha.text)
+          client.messages.send(domain: ENV['VK__DOMAIN'],
+                               message: message,
+                               captcha_sid: e.captcha_sid,
+                               captcha_key: captcha.text)
+        end
       end
     end
-  end
 
-  def send_email_message(order)
-    NotificationMailer.notify(order).deliver
-  end
+    def send_email_message(order)
+      NotificationMailer.notify(order).deliver
+    end
 
-  handle_asynchronously :send_vk_message
-  handle_asynchronously :send_email_message
+    handle_asynchronously :send_vk_message
+    handle_asynchronously :send_email_message
+  end
 end
